d0de5ee6ac87adcdd373975066b0be64210da88f
Adam Arthur <adamarthur44@gmail.com>
2021-05-18 16:40:31 +0100

    created a basic allEmployeses.js

---
+++ src/allEmployees.js
@@ -0,0 +1,64 @@
+const getAnswers = require("./getAnswers");
+const createManager = require("./createManager");
+const createIntern = require("./createIntern");
+const createEngineer = require("./createEngineer");
+
+const questionAndAnswers = async () => {
+    const questions = [
+        {
+            type: "list",
+            message: "What type of employee would you like to enter?",
+            name: "employee",
+            choices: [
+                {
+                    name: "Manager",
+                    value: "manager",
+                },
+                {
+                    name: "Engineer",
+                    value: "engineer",
+                },
+                {
+                    name: "Intern",
+                    value: "intern",
+                },
+                {
+                    name: "Exit",
+                    value: "exit",
+                },
+            ],
+        },
+    ];
+
+    const answers = await getAnswers(questions);
+
+    return answers;
+};
+
+const getAllEmployees = async () => {
+    const employees = [];
+
+    let inProgress = true;
+
+    while (inProgress) {
+        const answers = await questionAndAnswers();
+
+        if (answers.employee === "exit") {
+            inProgress = false;
+        } else {
+            if(answers.employee === "manager") {
+                const manager = await createManager();
+                employees.push(manager);
+            } else if (answers.employee === "engineer") {
+                const engineer = await createEngineer();
+                employees.push(engineer); 
+            } else if (answers.employee === "intern") {
+                const intern = await createIntern();
+                employees.push(intern); 
+            }
+        }
+    }
+    return employees;
+};
+
+module.exports = getAllEmployees;
