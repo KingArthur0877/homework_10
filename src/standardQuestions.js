5e666529be8c96ea5143d009ccf4a1e40a672d6b
Adam Arthur <adamarthur44@gmail.com>
2021-05-18 17:04:37 +0100

    added validation to employeeID and email

--- src/standardQuestions.js
+++ src/standardQuestions.js
@@ -8,11 +8,26 @@ const standardQuestions = [
         type: "number",
         message: "What is the employees ID number?",
         name: "employeeID",
+        validate: (id) => {
+            if (isNaN(id)) {
+              return 'Please only use numbers to set the employee ID.';
+            } else {
+              return true;
+            }
+        }
     },
     {
         type: "input",
         message: "What is the employees email address?",
         name: "email",
+        validate: (email) => {
+            emailValid = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(email);
+            if (emailValid === true) {
+              return true
+            } else {
+              return 'Please enter a valid email address.';
+            }
+        }
     },
 ];
 
