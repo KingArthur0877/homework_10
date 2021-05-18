d1b973d2d0572f97a964b1853f6ccc2a73ee7641
Adam Arthur <adamarthur44@gmail.com>
2021-05-18 15:33:33 +0100

    added standard questions

---
+++ src/standardQuestions.js
@@ -0,0 +1,19 @@
+const standardQuestions = [
+    {
+        type:"input",
+        message: "Please enter the employees name:",
+        name: "name",
+    },
+    {
+        type: "number",
+        message: "What is the employees ID number?",
+        name: "employeeID",
+    },
+    {
+        type: "input",
+        message: "What is the employees email address?",
+        name: "email",
+    },
+];
+
+module.exports = standardQuestions;
